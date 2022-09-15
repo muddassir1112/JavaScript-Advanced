c4731a82da3fb70e5d10eccb34eeeda2dabf9f8a
muddassir1112 <muddassiransari2022aug@cedcoss.com>
2022-09-15 10:01:45 +0530

    Apply employee name validation (not be empty, not a number). Use try, catch and throw and display the proper error message if any during validation.

---
+++ src/script9.js
@@ -0,0 +1,39 @@
+var data=[];
+function add() {
+    let id = document.getElementById("id").value;
+    let name = document.getElementById("name").value;
+    let salary = document.getElementById("salary").value;
+    try{
+  if(name == "") throw "Empty field not allow"
+  if(!isNaN(name))throw "Number not reuiqred in Name Field"
+    var val = {
+      ID: id,
+      Name: name,
+      Salary: salary,
+    };
+    data.push(val);
+  
+    display();
+  
+  }
+  catch(err){
+    document.getElementById('demo').innerHTML= err;
+  }
+}
+  function display() {
+    var table =
+      "<table><tr><th>ID</th><th>Name</th><th>Salary</th>";
+    data.forEach((element) => {
+      table +=
+        "<tr><td>" +
+        element.ID +
+        "</td><td>" +
+        element.Name +
+        "</td><td>" +
+        element.Salary +
+        "</td><td>";
+        
+    });
+    table += "</table>";
+    document.getElementById("demo").innerHTML = table;
+  }
